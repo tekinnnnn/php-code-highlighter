ff059f0547f0e6c292e36eba645fb7dcc30fef11
tekinnnnn <tekinaydogdu01@gmail.com>
2015-03-24 23:16:41 +0200

    class

--- index.php
+++ index.php
@@ -12,93 +12,52 @@
 	</h1>
 	<div class="page">
 <?php
-
-class sytax_highlighter
-{
-
-    function __construct($file, $isfile = true)
-    {
-        
-        if (is_file($file)) {
-            $code = file($file);
-            $code = implode("", $code);
-        } else {
-            $code = $file;
-        }
-        
-        $code = htmlspecialchars_decode($code);
-        $code = highlight_string($code, true);
-        $code = explode("\r", $code);
-        
-        foreach ($code as $line => $data) {
-            $td1[] = '<div class="line">' . $line ++ . '</div>';
-            $dat = substr($data, 6) == "" ? "&nbsp;" : substr($data, 6);
-            
-            $td2[] = '<div class="code">' . $dat . '</div>';
-        }
-        
-        $line = implode("\r", $td1);
-        $code = implode("\r", $td2);
-        $tr = array(
-            $line,
-            $code
-        );
-        
-        ?>
-
-    <div class="highlighter">
-		<div id="line"><?php echo $line; ?></div>
-		<div id="code"><?php echo $code; ?></div>
-	</div>
-        <?php
-    }
-}
-
+require_once 'class.php';
 $syntax = new sytax_highlighter("text.txt");
 
 /*
-$syntax = new sytax_highlighter('<?php
-
-class sytax_highlighter
-{
-
-    function __construct($file, $isfile = true)
-    {
-        
-        if (is_file($file)) {
-            $code = file($file);
-            $code = implode("", $code);
-        } else {
-            $code = $file;
-        }
-        
-        $code = htmlspecialchars_decode($code);
-        $code = highlight_string($code, true);
-        $code = explode("\r", $code);
-        
-        foreach ($code as $line => $data) {
-            $td1[] = \'<div class="line">\' . $line ++ . \'</div>\';
-            $dat = substr($data, 6) == "" ? "&nbsp;" : substr($data, 6);
-            
-            $td2[] = \'<div class="code">\' . $dat . \'</div>\';
-        }
-        
-        $line = implode("\r", $td1);
-        $code = implode("\r", $td2);
-        $tr = array(
-            $line,
-            $code
-        );
-
-    echo \'<div class="highlighter">
-		<div id="line">\'.$line.\'</div>
-		<div id="code">\'.$code;.\'</div>
-	</div>\';
-    }
-}
-    
-?>');
-*/
+ * $syntax = new sytax_highlighter('<?php
+ *
+ * class sytax_highlighter
+ * {
+ *
+ * function __construct($file, $isfile = true)
+ * {
+ *
+ * if (is_file($file)) {
+ * $code = file($file);
+ * $code = implode("", $code);
+ * } else {
+ * $code = $file;
+ * }
+ *
+ * $code = htmlspecialchars_decode($code);
+ * $code = highlight_string($code, true);
+ * $code = explode("\r", $code);
+ *
+ * foreach ($code as $line => $data) {
+ * $td1[] = \'<div class="line">\' . $line ++ . \'</div>\';
+ * $dat = substr($data, 6) == "" ? "&nbsp;" : substr($data, 6);
+ *
+ * $td2[] = \'<div class="code">\' . $dat . \'</div>\';
+ * }
+ *
+ * $line = implode("\r", $td1);
+ * $code = implode("\r", $td2);
+ * $tr = array(
+ * $line,
+ * $code
+ * );
+ *
+ * echo \'<div class="highlighter">
+ * <div id="line">\'.$line.\'</div>
+ * <div id="code">\'.$code;.\'</div>
+ * </div>\';
+ * }
+ * }
+ *
+ * ?>');
+ */
 
 ?>
 </div>
