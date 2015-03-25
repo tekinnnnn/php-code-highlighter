3e85f5c5d68a3223fb22567fa6c98fe288f02fde
Tekin Aydoğdu <tekinaydogdu01@gmail.com>
2015-03-25 16:35:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,50 +15,6 @@
 require_once 'class.php';
 $syntax = new sytax_highlighter("text.txt");
 
-/*
- * $syntax = new sytax_highlighter('<?php
- *
- * class sytax_highlighter
- * {
- *
- * function __construct($file, $isfile = true)
- * {
- *
- * if (is_file($file)) {
- * $code = file($file);
- * $code = implode("", $code);
- * } else {
- * $code = $file;
- * }
- *
- * $code = htmlspecialchars_decode($code);
- * $code = highlight_string($code, true);
- * $code = explode("\r", $code);
- *
- * foreach ($code as $line => $data) {
- * $td1[] = \'<div class="line">\' . $line ++ . \'</div>\';
- * $dat = substr($data, 6) == "" ? "&nbsp;" : substr($data, 6);
- *
- * $td2[] = \'<div class="code">\' . $dat . \'</div>\';
- * }
- *
- * $line = implode("\r", $td1);
- * $code = implode("\r", $td2);
- * $tr = array(
- * $line,
- * $code
- * );
- *
- * echo \'<div class="highlighter">
- * <div id="line">\'.$line.\'</div>
- * <div id="code">\'.$code;.\'</div>
- * </div>\';
- * }
- * }
- *
- * ?>');
- */
-
 ?>
 </div>
 </body>
